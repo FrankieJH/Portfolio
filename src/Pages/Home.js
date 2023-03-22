ee4f83b2e43d71a7cee11103aaf2000b98224f33
Francisco Hernandez <kikohernandez91@yahoo.com>
2023-03-21 22:13:14 -0700

    added assets and added hero image

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,7 +4,10 @@ import '../Styling/Home.css';
 function Home(){
     return(
         <div className="Home">
-            
+            {/* hero section container  */}
+            <div className="hero_section">
+                <img className="hero_image" src={require('../Assets/HeroImg.jpg')} />
+            </div>
         </div>
     )
 }
