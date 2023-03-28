630f94c48cba89373be3b7273afeef49267b6f75
Francisco Hernandez <kikohernandez91@yahoo.com>
2023-03-27 23:35:48 -0700

    changed classNames, moved import to index.html and fixed identations

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,9 +4,9 @@ import '../Styling/Home.css';
 function Home(){
     return(
         <div className="Home">
-            {/* hero section container  */}
-            <div className="hero_section">
-                <img className="hero_image" src={require('../Assets/HeroImg.jpg')} />
+            {/* Hero section container  */}
+            <div className="Hero_section">
+                <img className="Hero_image" src={require('../Assets/HeroImg.jpg')} />
             </div>
         </div>
     )
