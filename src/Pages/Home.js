2937c43fbf6e6ae5995766574212c84a576052ba
Francisco Hernandez <kikohernandez91@yahoo.com>
2023-03-30 14:07:12 -0700

    cleaned up the code and changed classnames

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -8,17 +8,17 @@ function Home(){
             <div className="Hero_section">
             <img className="Hero_image" src= {require('../Assets/HeroImg.jpg')} />
                 <div className="Hero_overlay">
-                    <p className="style_2">Hello, my name is</p>
-                    <h1 className="style_1">FRANKIE <span id="style_1">HRNDZ</span></h1>
-                    <p className="style_2">I am passionate about crafting visually stunning and 
-                    engaging web experiences. With a keen eye for design and a 
-                    proficiency in modern front-end technologies, I strive to create
+                    <p className="Style_2">Hello, my name is</p>
+                    <h1 className="Style_1">FRANKIE <span id="Style_1_transparent">HRNDZ</span></h1>
+                    <p className="Style_2">I am passionate about crafting visually stunning and 
+                     engaging web experiences. With a keen eye for design and a 
+                     proficiency in modern front-end technologies, I strive to create
                      intuitive and responsive interfaces that enhance user engagement 
                      and satisfaction.</p>
                 </div>
-                <div className="scroll_indicator" >
-                    <p className="style_2">SCROLL DOWN</p>
-                    <img className="indicator_image" src={require('../Assets/next.png')} />
+                <div className="Scroll_indicator" >
+                    <p className="Style_2">SCROLL DOWN</p>
+                    <img className="Indicator_image" src={require('../Assets/next.png')} />
                 </div>
 
             </div>
