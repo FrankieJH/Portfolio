a7e0c32f28a089c6a69518c088e45b785dfcf425
Francisco Hernandez <kikohernandez91@yahoo.com>
2023-05-15 22:14:31 -0700

    created about me section

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -22,6 +22,43 @@ function Home(){
                 </div>
 
             </div>
+            {/* About Me Section  */}
+            <div className="About_section">
+                <div className="About_me_container">
+                    <h2> <span>00.</span> About Me</h2>
+                    <p> I am a highly motivated and passionate <span>software engineer</span> with a 
+                        deep love for problem-solving. Using my expertise in modern programming 
+                        languages and technologies, I enjoy tackling complex technical 
+                        challenges and developing innovative solutions that exceed expectations.
+                        My strong communication and collaboration skills allow me to work 
+                        effectively with colleagues and stakeholders to ensure project success.
+                        <br/><br/>
+                        With a commitment to <span>excellence</span> and a keen eye for detail, 
+                        I take great pride in delivering high-quality work that meets 
+                        or exceeds expectations. As a self-motivated individual, I am 
+                        constantly seeking new challenges and opportunities to learn and grow, 
+                        both personally and professionally. 
+                        <br/><br/>
+                        My dedication to my craft has earned me a reputation as a reliable and 
+                        dependable team player who is always willing to go the extra mile.
+                    </p>
+                    <div className="About_skills_container">
+                        <ul>
+                            <li>React.js</li>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                            <li>JavaScript</li>
+                            <li>Redux</li>
+                            <li>Git</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="About_image_container" >
+                    <img src={require('../Assets/Profile_placeholder.jpeg')} 
+                    alt="Image of Frankie"/>
+                    <button>Resume</button>
+                </div>
+            </div>
         </div>
     )
 }
