e03576cde7c2f77310c3066e8230d0991ee54008
Francisco Hernandez <kikohernandez91@yahoo.com>
2023-07-17 18:37:44 -0700

    updated line of code

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -14,12 +14,12 @@ function Home(){
                 <div className="Hero_center">
                     <p className="Font_style_2">Hello, my name is</p>
                     <h1 className="Font_style_1" 
-                        id="Hero_center_child_2">
+                    id="Hero_center_child_2">
                         FRANKIE<span id="Font_style_1_transparent">HRNDZ</span>
                     </h1>
                     <p className="Font_style_2"
-                       id="Hero_center_child_3">
-                       I am passionate about crafting visually stunning and 
+                    id="Hero_center_child_3">
+                     I am passionate about crafting visually stunning and 
                      engaging web experiences. With a keen eye for design and a 
                      proficiency in modern front-end technologies, I strive to create
                      intuitive and responsive interfaces that enhance user engagement 
