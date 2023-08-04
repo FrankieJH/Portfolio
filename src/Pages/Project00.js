a4d5bae8f5147b9edcadacd0de5aedae650da994
Francisco Hernandez <kikohernandez91@yahoo.com>
2023-08-04 16:22:09 -0700

    added main section for this page

--- src/Pages/Project00.js
+++ src/Pages/Project00.js
@@ -6,11 +6,11 @@ function Project00(){
     return(
         <div className="Project00">
             {/* Hero section  */}
-            <div className="Project00_hero_section">
-                <img className="Project00_hero_image"
+            <div className="Project_hero_section">
+                <img className="Project_hero_image"
                 src={require('../Assets/project_image1_placeholder.png')}
                 alt=""/>
-                <div className="Project00_hero_container">
+                <div className="Project_hero_container">
                     <h2 className="Font_style_2">
                         Project Name Here
                     </h2>
@@ -28,6 +28,59 @@ function Project00(){
                         Whether you want to explore a new idea or just say hello, 
                         I'm always open to new opportunities.
                     </p>
+                </div>
+            </div>
+            {/*  main section*/}
+            <div className="Project_main_section">
+                <div className="Main_top">
+                    <div className="Main_top_image_wrapper">
+                        <img className="Main_top_image_1"
+                        src={require('../Assets/project_image02_placeholder.png')}
+                        alt=""/>
+                    </div>
+                    <div className="Main_top_wrapper">
+                        <ul className="Font_style_4">
+                            <li>
+                                Github:<br/>
+                                <img src={require('../Assets/code-branch.png')} 
+                                alt=""/>
+                            </li>
+                            <li>
+                                Preview:<br/>
+                                <img src={require('../Assets/preview.png')} 
+                                alt=""/>
+                            </li>
+                            <li>
+                                Preview:<br/>
+                                React, JavaScript,<br/> HTML, Node.js
+                            </li>
+                        </ul>
+                        <p className="Font_style_3">
+                            Feel free to get in touch with me to discuss 
+                            any potential projects or collaborations. Whether
+                            you want to explore a new idea or just say hello, 
+                            I'm always open to new opportunities.
+                        </p>
+                    </div>
+                </div>
+                <div className="Main_bottom">
+                    <div className="Main_bottom_image_wrapper">
+                        <img 
+                        src={require('../Assets/project_image04_placeholder.png')}
+                        alt=""/>
+                    </div>
+                    <div className="Main_bottom_wrapper">
+                        <h2 className="Font_style_4">
+                            Some title here
+                        </h2>
+                        <p className="Font_style_3">
+                            Feel free to get in touch with me to discuss 
+                            any potential projects or collaborations. 
+                            Whether you want to explore a new idea or 
+                            just say hello, I'm always open to new
+                            opportunities.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
